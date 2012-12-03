5a4d9d624d9f221015ec9fd4ecc48d0e58b7a9c4
Hugh Nougher <hughnougher@gmail.com>
2012-12-04 10:40:59 +1100

    Added MPL2

--- animate-page-complex.js
+++ animate-page-complex.js
@@ -1,4 +1,10 @@
-﻿
+﻿/**
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 var Point = function (X, Y) {
 	this._X = X;
 	this._Y = Y;
